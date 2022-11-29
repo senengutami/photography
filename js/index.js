5b0fec46e627951e0c933c9bd833e5121be6151d
senengutami <senengutami2@gmail.com>
2022-11-29 19:10:45 +0545

    fix scroll

--- js/index.js
+++ js/index.js
@@ -71,3 +71,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //do work
     showOnlyChosen("hk")
 });
+
+let touchstartX = 0
+let touchendX = 0
+
+function checkDirection() {
+    if (touchendX < touchstartX) alert('swiped left!')
+    if (touchendX > touchstartX) alert('swiped right!')
+}
+
+document.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX
+    checkDirection()
+})
